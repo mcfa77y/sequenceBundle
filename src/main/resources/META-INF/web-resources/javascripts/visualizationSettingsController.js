466420a309f8552aaacc095568dcc84e1b7fccbc
Joe Lau <mcfa77y@gmail.com>
2015-03-25 11:37:35 +0000

    jetty version of sb webga!

---
+++ src/main/resources/META-INF/web-resources/javascripts/visualizationSettingsController.js
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(function () {
+    'use strict';
+
+    $("#visualSettingsForm").submit(function (event) {
+        // Stop form from submitting normally
+        event.preventDefault();
+        // Get some values from elements on the page:
+        var $form = $(this),
+                url = $form.attr("action");
+        var data = Utils.createData(
+                {alignmentType: $('#alignmentType').val()});
+
+        
+        // Send the data using post
+        var posting = $.post(url, data);
+        // Put the results in a div
+        posting.done(function (data) {
+            var d = new Date();
+            var wp = data["webPath"] + "?" + d.getTime();
+            var filename = Utils.getFilename(wp);
+            Utils.jobStatusPoll(filename, wp);
+            // number of columns may have been updated due to new column width
+            $('#visualSettingsForm #columnCount').val(data.numberOfColumns);
+        });
+    });
+
+})
