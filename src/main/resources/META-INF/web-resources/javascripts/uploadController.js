a9cb141c6a0baccbd8f4e867cdc1832be4ab256d
Joe Lau <mcfa77y@gmail.com>
2015-06-04 17:52:00 +0100

    changed contains to include

--- src/main/resources/META-INF/web-resources/javascripts/uploadController.js
+++ src/main/resources/META-INF/web-resources/javascripts/uploadController.js
@@ -61,7 +61,7 @@ var sequence = "";
         // handle possible errors in data
         if (data.errorMessage && data.errorMessage.length > 0) {
             var errorMessage = '';
-            if (data.errorMessage.contains("1000")) {
+            if (data.errorMessage.includes("1000")) {
                 errorMessage = "FASTA format is valid, but your data is too large (it has [" + data.sequenceCount + "] sequences, each [" + data.sequenceBases + "] positions long).";
                 uploadSequenceInfo('green', errorMessage, false);
             } else {
